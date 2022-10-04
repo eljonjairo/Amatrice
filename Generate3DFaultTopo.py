d619bccc1c8e1ea44258a67ce2b0a4ac11a28c54
John Diaz <eljonjairo@gmail.com>
2022-10-04 11:32:12 -0500

    Falla acragada y ploteada con topo

--- Generate3DFaultTopo.py
+++ Generate3DFaultTopo.py
@@ -1,3 +1,4 @@
+#!/home/jon/anaconda3/bin/python
 
 #
 # Generate Fault and Topography output files
@@ -6,39 +7,51 @@
 
 # To Do List:
 #    Add index from hypocenter coordinates
-#    Test python dealunay triangulation.
+#    Gempy.
 #
 
 import os
 import warnings
 
 import numpy as np
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 from scipy import interpolate
 from scipy.spatial import Delaunay
 import pickle
-
+from pathlib import Path
 import pygmt
 import utm
+import pandas as pd
+import matplotlib.colors
+from matplotlib import cm
+
 
 warnings.filterwarnings("ignore")
 os.system('clear')
 
 
 dhF = 0.5  # Output subfaults size in Km
-infile = 'Input/s2016AMATRI01PIZZ.fsp'  # Input file with fault data
+inDir  = Path('Input/')
+outDir = Path('Outputs/3DFaults/')
+
+infile = 's2016AMATRI01PIZZ.fsp'  # Input file with fault data
 name = "AmatricePizzi2017_dhF"  # Output files name
 
 # Dimension of input fault
 nstkin = 30
 ndipin = 12
 
+# Lon-Lat limits for Topo Download
+Latmin = 42.1
+Latmax = 43.3
+Lonmin = 12.4
+Lonmax = 13.9
+
 # X and Y UTM limits (Km) for Model
-xmin = 290.0
-xmax = 410.0
-ymin = 4670.0
-ymax = 4790.0
+xmin = 300.0
+xmax = 400.0
+ymin = 4680.0
+ymax = 4780.0
 zmin = -60.0
 
 # Hypocenter coordinates (Km) 
@@ -46,14 +59,109 @@ hypolon = 13.25
 hypolat = 42.70
 hypoz =-5.0
 
+# m to Km
+m = 1000
+
 print("  ")
 print(" START PROGRAM ")
 print("  ")
 
+# Load Earth relief data for the entire globe and a subset region
+region = [Lonmin,Lonmax,Latmin,Latmax]
+grid = pygmt.datasets.load_earth_relief(resolution="03s", region=region)
+
+TopoZMat = grid.data
+TopoLat = grid.lat.data
+TopoLon = grid.lon.data
+
+zmax = np.max(TopoZMat)/m
+
+TopoLonMat, TopoLatMat = np.meshgrid(TopoLon,TopoLat)
+TopoXMat,TopoYMat, tmp1, tmp2 = utm.from_latlon(TopoLatMat, TopoLonMat,33,'N')
+TopoXMat = TopoXMat/m
+TopoYMat = TopoYMat/m
+
+#Load Stations input file
+inStats =  pd.read_csv(inDir.joinpath("stations.dat"), delimiter= '\s+')
+
+inStatsX, inStatsY, tmp1, tmp2 = utm.from_latlon(inStats.Lat, inStats.Lon,33,'N')
+inStats['X'] = inStatsX/m
+inStats['Y'] = inStatsY/m
+
+fig = pygmt.Figure()
+pygmt.makecpt(cmap="geo", series=[-10000, 10000])
+fig.grdimage(grid=grid, region=region,
+             frame=['WSrt+t" Topografia Italia Central"', "xa0.4", "ya0.4"])
+fig.colorbar(frame=["xa2000f500+lElevación ", "y+lm"])
+#stations
+fig.plot(x=inStats.Lon, y=inStats.Lat,style="t0.5c",color='red', pen="black") 
+fig.text(textfiles=None,x=inStats.Lon-0.05, y=inStats.Lat+0.05, position=None,text=inStats.Name,  
+ angle=0, font='9p,Helvetica-Bold,black', justify='LM')
+
+fig.show()
+
+cvals  = [ 0, 100, 1000, 2000, 3000, 4000, ]
+colors = ["darkgreen","green","forestgreen","yellowgreen",
+          "orange","maroon","sienna","brown","white"]
+
+norm=plt.Normalize(min(cvals),max(cvals))
+tuples = list(zip(map(norm,cvals), colors))
+cTopo = matplotlib.colors.LinearSegmentedColormap.from_list("", tuples)
+
+#Load Fault input file
+infile = inDir.joinpath(infile)
+
+inData = np.loadtxt(infile, skiprows = 50)
+LatFinMat = np.reshape(inData[:,0], (ndipin, nstkin))
+LonFinMat = np.reshape(inData[:,1], (ndipin, nstkin))
+ZFinMat = np.reshape(inData[:,4], (ndipin, nstkin))
+SlipinMat = np.reshape(inData[:,5], (ndipin, nstkin))
+
+XFinMat,YFinMat, tmp1, tmp2 = utm.from_latlon(LatFinMat, LonFinMat,33,'N')
+XFinMat = XFinMat/m
+YFinMat = YFinMat/m
+
+fig = plt.figure(figsize = (10,10))
+ax = fig.subplots(1,1)
+fp=ax.pcolormesh(XFinMat,YFinMat,SlipinMat, cmap="viridis")
+plt.colorbar(fp,location='bottom',label="Slip (m)",shrink=.6)
+
+fig = plt.figure(figsize = (10,10))
+ax = fig.subplots(1,1)
+mp=ax.pcolormesh(TopoXMat,TopoYMat,TopoZMat*m, cmap=cTopo)
+plt.colorbar(mp,location='bottom',label="Elevación (m)",shrink=.6)
+fp=ax.pcolormesh(XFinMat,YFinMat,SlipinMat, cmap='viridis')
+plt.colorbar(fp,location='right',label="Slip (m)",shrink=.6)
+ax.scatter(inStats.X,inStats.Y,c ="red",linewidths = 0.5,marker ="^",edgecolor ="black",s = 80)
+for i in range(0,inStats.X.size):
+    ax.text(inStats.X[i]+0.7, inStats.Y[i]+0.5, inStats.Name[i],fontsize=10,fontweight='bold')
+
+ax.set_xlabel(" X (Km)")
+ax.set_ylabel(" Y (Km)")
+ax.set_xlim(xmin,xmax)
+ax.set_ylim(ymin,ymax)
+ax.set_aspect('equal',adjustable='box')
+ax.set_title('Topografía Italia Central')
+
+fig = plt.figure()
+ax = plt.axes(projection ='3d')
+
+surf = ax.plot_surface( XFinMat, YFinMat, ZFinMat, facecolors=cm.viridis(SlipinMat), linewidth=0,
+                        antialiased=False )
+plt.colorbar(surf,location='top',label="Slip (m)",shrink=.6)
+ax.set_title("Input Slip")
+ax.set_xlabel(" X (Km)")
+ax.set_ylabel(" Y (Km)")
+ax.set_zlabel(" Z (Km)")
+ax.set_xlim(xmin,xmax)
+ax.set_ylim(ymin,ymax)
+ax.azim = 0
+ax.dist = 10
+ax.elev = 0
+plt.show()
 
 
-
-
-
-
+print("  ")
+print(" END PROGRAM ")
+print("  ")
 
